4c42f0f5f5570151818ea023e7704f9ec2b27ef2
Ninkochan <nitipat_n@cmu.ac.th>
2025-01-25 14:00:57 +0700

    B r

--- my_operations.py
+++ my_operations.py
@@ -10,8 +10,7 @@ def add(num1: int, num2: int) -> None:
 def diff(num1: int, num2: int) -> None:
     # Edit code under this line
 
-    print(num1, "-", num2, "is", res)
-    pass
+    print(num1, "-", num2, "is", num1-num2)
 
 
 # Member C: edit this function.
